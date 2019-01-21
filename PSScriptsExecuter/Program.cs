e2d2c0750602414fdd3e2685cf85aa3d64af6d5f
Manan Habib <mananhabib31@gmail.com>
2019-01-21 18:41:29 +0500

    Added installer, ps scripts and scripts executer

---
+++ PSScriptsExecuter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSScriptsExecuter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (PowerShell powershell = PowerShell.Create())
+            {
+                
+                PSCommand new1 = new PSCommand();
+                
+                GetPSScriptsFilesPaths().ForEach(path =>
+                {
+                    powershell.AddScript(@System.IO.File.ReadAllText(path));
+
+                    Collection<PSObject> results = powershell.Invoke();
+
+                });
+
+                Process process = new Process();
+                // Configure the process using the StartInfo properties.
+                process.StartInfo.FileName = @"gpmc.msc";
+                process.Start();
+
+
+            }
+        }
+
+        static List<string> GetPSScriptsFilesPaths()
+        {
+            var executingPath = $@"{ Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location)}\PS Scripts";
+
+            return Directory.GetFiles(executingPath).ToList();
+        }
+    }
+}
