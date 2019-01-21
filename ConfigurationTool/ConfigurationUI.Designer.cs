4aa1b5fdbf2a320b3a5897e40f7f121cfb70b618
Manan Habib <mananhabib31@gmail.com>
2019-01-21 18:38:20 +0500

    Added configuration tool in installer

---
+++ ConfigurationTool/ConfigurationUI.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ConfigurationTool
+{
+    partial class ConfigurationUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigurationUI));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLdapPath = new System.Windows.Forms.TextBox();
+            this.txtWebURL = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LDAP Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Target WEB URL";
+            // 
+            // txtLdapPath
+            // 
+            this.txtLdapPath.Enabled = false;
+            this.txtLdapPath.Location = new System.Drawing.Point(107, 36);
+            this.txtLdapPath.Name = "txtLdapPath";
+            this.txtLdapPath.Size = new System.Drawing.Size(349, 20);
+            this.txtLdapPath.TabIndex = 2;
+            // 
+            // txtWebURL
+            // 
+            this.txtWebURL.Location = new System.Drawing.Point(107, 83);
+            this.txtWebURL.Name = "txtWebURL";
+            this.txtWebURL.Size = new System.Drawing.Size(349, 20);
+            this.txtWebURL.TabIndex = 3;
+            this.txtWebURL.Text = "https://";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(381, 161);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(107, 123);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(349, 20);
+            this.txtToken.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Token";
+    
+            // 
+            // ConfigurationUI
+            // 
+            this.ClientSize = new System.Drawing.Size(481, 196);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtWebURL);
+            this.Controls.Add(this.txtLdapPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ConfigurationUI";
+            this.Text = "Confiigurations";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLdapPath;
+        private System.Windows.Forms.TextBox txtWebURL;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtToken;
+    }
+}
+
---
+++ ConfigurationTool/ConfigurationUI.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ConfigurationTool
+{
+    public partial class ConfigurationUI : Form
+    {
+        string _serviceName = string.Empty;
+        public ConfigurationUI(string serviceName)
+        {
+            this._serviceName = serviceName;
+
+            InitializeComponent();
+
+            //SetInfoIfServiceNameNotGiven();
+            SetJoinedDomainPath();
+        }
+
+        private void SetInfoIfServiceNameNotGiven()
+        {
+            //SetJoinedDomainPath();
+            //if (string.IsNullOrEmpty(this._serviceName))
+            //{
+            //    var executingDirectoryPath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+            //    var configFilePath = $@"{executingDirectoryPath}\LogonEventsWatcherService.exe.config";
+            //}
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtLdapPath.Text))
+            {
+                errorProvider.SetError(txtLdapPath, "Please give Ldap Path.");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtWebURL.Text) || !IsValidWEBURL(txtWebURL.Text))
+            {
+                errorProvider.SetError(txtWebURL, "Please give valid Web URL.");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtToken.Text))
+            {
+                errorProvider.SetError(txtToken, "Please give Ldap Path.");
+                return;
+            }
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(GetServiceConfigFilePath());
+
+            foreach (XmlNode element in xmlDocument.GetElementsByTagName("add"))
+            {
+                foreach (XmlAttribute attribute in element.Attributes)
+                {
+                    if (attribute.Name == "key" && attribute.Value == "LdapPath")
+                    {
+                        foreach (XmlAttribute attributeToUpdate in element.Attributes)
+                        {
+                            if (attributeToUpdate.Name == "value")
+                            {
+                                attributeToUpdate.Value = txtLdapPath.Text;
+                            }
+                        }
+                    }
+                    if (attribute.Name == "key" && attribute.Value == "TargetWebServiceUrl")
+                    {
+                        foreach (XmlAttribute attributeToUpdate in element.Attributes)
+                        {
+                            if (attributeToUpdate.Name == "value")
+                            {
+                                attributeToUpdate.Value = txtWebURL.Text;
+                            }
+                        }
+                    }
+                    if (attribute.Name == "key" && attribute.Value == "AuthToken")
+                    {
+                        foreach (XmlAttribute attributeToUpdate in element.Attributes)
+                        {
+                            if (attributeToUpdate.Name == "value")
+                            {
+                                attributeToUpdate.Value = txtToken.Text;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            xmlDocument.Save(GetServiceConfigFilePath());
+
+            ServiceController sc = new ServiceController(this._serviceName);
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                sc.Stop();
+                sc.WaitForStatus(ServiceControllerStatus.Stopped);
+                sc.Start();
+            }
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+        
+
+        string GetServiceConfigFilePath()
+        {
+            return $@"{System.IO.Path.GetDirectoryName(Application.ExecutablePath)}\LogonEventsWatcherService.exe.config";
+            //using (ManagementObject wmiService = new ManagementObject("Win32_Service.Name='" + _serviceName + "'"))
+            //{
+            //    wmiService.Get();
+            //    string currentserviceExePath = wmiService["PathName"].ToString();
+            //    return $"{wmiService["PathName"].ToString().Replace("\"", "")}.config";
+            //}
+        }
+        void SetJoinedDomainPath()
+        {
+            var domainName = IPGlobalProperties.GetIPGlobalProperties().DomainName;
+            domainName = string.Join(",", domainName.Split(".".ToArray(), StringSplitOptions.RemoveEmptyEntries).Select(x => $"dc={x}"));
+
+            txtLdapPath.Text = $"LDAP://{domainName}";
+        }
+
+        bool IsValidWEBURL(string URLString)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(URLString, UriKind.Absolute, out uriResult)
+                   && (uriResult.Scheme == Uri.UriSchemeHttp
+                   || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        
+    }
+}
